1ed86d902028b70895d53bf37a2ebeb6139ca331
Jerry Chong <jerrychong25@gmail.com>
2020-02-16 23:01:19 +0800

    Added getStates()

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 module.exports = (str) => {
     return `Installation success for malaysia-list library. Your text: ${str}`;
 }
+
+module.exports.getStates = (language) => {
+    return {
+        states: [
+            'Johor Darul Ta\'zim',
+            'Kedah Darul Aman',
+            'Kelantan Darul Naim',
+            'Malacca',
+            'Negeri Sembilan Darul Khusus',
+            'Pahang Darul Makmur',
+            'Penang',
+            'Perak Darul Ridzuan',
+            'Perlis Indera Kayangan',
+            'Sabah',
+            'Sarawak',
+            'Selangor Darul Ehsan',
+            'Terengganu Darul Iman'
+        ]
+    }
+}
