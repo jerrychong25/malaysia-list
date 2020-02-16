47c578f4e6b9eb6f8e4b66da44f1ef3ac40de972
Jerry Chong <jerrychong25@gmail.com>
2020-02-16 23:11:58 +0800

    Added getStates() languages option (en, ms, zh)

--- index.js
+++ index.js
@@ -3,21 +3,46 @@ module.exports = (str) => {
 }
 
 module.exports.getStates = (language) => {
-    return {
-        states: [
-            'Johor Darul Ta\'zim',
-            'Kedah Darul Aman',
-            'Kelantan Darul Naim',
-            'Malacca',
-            'Negeri Sembilan Darul Khusus',
-            'Pahang Darul Makmur',
-            'Penang',
-            'Perak Darul Ridzuan',
-            'Perlis Indera Kayangan',
-            'Sabah',
-            'Sarawak',
-            'Selangor Darul Ehsan',
-            'Terengganu Darul Iman'
-        ]
+
+    let statesList
+
+    if (language === 'en' | language === 'ms') {
+        statesList = {
+            states: [
+                'Johor Darul Ta\'zim',
+                'Kedah Darul Aman',
+                'Kelantan Darul Naim',
+                'Malacca',
+                'Negeri Sembilan Darul Khusus',
+                'Pahang Darul Makmur',
+                'Penang',
+                'Perak Darul Ridzuan',
+                'Perlis Indera Kayangan',
+                'Sabah',
+                'Sarawak',
+                'Selangor Darul Ehsan',
+                'Terengganu Darul Iman'
+            ]
+        }
+    } else if (language === 'zh') {
+        statesList = {
+            states: [
+                '柔佛',
+                '吉打',
+                '吉兰丹',
+                '马六甲',
+                '森美兰',
+                '彭亨',
+                '槟城',
+                '霹雳',
+                '玻璃市',
+                '沙巴',
+                '砂拉越',
+                '雪兰莪',
+                '登嘉楼'
+            ]
+        }
     }
+
+    return statesList
 }
