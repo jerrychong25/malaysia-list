ece2164223e56ef4d77374dd6cf3f5166fc008c2
Jerry Chong <jerrychong25@gmail.com>
2020-02-18 23:01:24 +0800

    Added logger

---
+++ helpers/logger.js
@@ -0,0 +1,11 @@
+module.exports.log = function (msg) { 
+    console.log(msg);
+};
+
+module.exports.info = function (msg) { 
+    console.info(msg);
+};
+
+module.exports.error = function (msg) { 
+    console.error(msg);
+};
