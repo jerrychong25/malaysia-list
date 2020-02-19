40aed50e065c0de18ef2e9e1c1da37c639a76a60
Jerry Chong <jerrychong25@gmail.com>
2020-02-19 23:51:24 +0800

    Added getPublicUniversities.js

---
+++ core/getPublicUniversities.js
@@ -0,0 +1,35 @@
+const global = require('../helpers/global')
+const { LANGUAGES } = require('../helpers/constants')
+
+module.exports = () => {
+    let publicUniversitiesList
+
+    if (global.getDefaultLanguage() === LANGUAGES.ENGLISH) {
+        publicUniversitiesList = {
+            public_universities: [
+                'University of Malaya',
+                'National University of Malaysia',
+                'Putra University, Malaysia',
+                'University of Science, Malaysia',
+                'University of Technology, Malaysia',
+                'Northern University of Malaysia',
+                'International Islamic University Malaysia',
+                'University of Malaysia Pahang',
+                'University of Malaysia Perlis',
+                'University of Malaysia Sabah',
+                'University of Malaysia Sarawak',
+                'University of Malaysia Terengganu',
+                'Sultan Idris Education University',
+                'University of Malaysia Kelantan',
+                'National Defence University of Malaysia',
+                'Islamic Science University of Malaysia',
+                'Sultan Zainal Abidin University',
+                'Technical University of Malaysia Malacca',
+                'Tun Hussein Onn University of Malaysia',
+                'MARA University of Technology'
+            ]
+        }
+    }
+
+    return publicUniversitiesList
+}
