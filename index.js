866ee122615419e30335c575d668bbf6067fc411
Jerry Chong <jerrychong25@gmail.com>
2020-03-16 22:36:07 +0800

    Added getPrimeMinisters.js

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const global = require('./helpers/global')
 // Core Functions Source
 const getStates = require('./core/getStates')
 const getFederalTerritories = require('./core/getFederalTerritories')
+const getPrimeMinister = require('./core/getPrimeMinisters')
 const getPublicUniversities = require('./core/getPublicUniversities')
 
 // Init Function
 module.exports.init = (defaultLanguage) => {
-    global.setDefaultLanguage(defaultLanguage)
+  global.setDefaultLanguage(defaultLanguage)
 }
 
 // Core Functions
 module.exports.getStates = getStates
 module.exports.getFederalTerritories = getFederalTerritories
+module.exports.getPrimeMinister = getPrimeMinister
 module.exports.getPublicUniversities = getPublicUniversities
